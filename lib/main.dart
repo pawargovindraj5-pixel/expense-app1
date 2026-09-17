3a8f2f042bf012977ff7880a4a3de8d4bee5e3f3
Varun Patil <varunbpatil1121@gmail.com>
2026-09-17 14:01:40 +0530

    added feature changes

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
     ),
     Expense(
       title: 'Cinema',
-      amount: 15.69,
+      amount: 1.69,
       date: DateTime.now(),
       category: Category.leisure,
     ),
